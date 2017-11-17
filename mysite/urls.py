397c8078911abe1b4f943823db3bc9e6bee34215
kjm4311 <kjm4311@naver.com>
2017-11-17 15:13:59 +0900

    comment

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views
 
-from django.contrib.auth import views
-
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', views.login, name='login'),

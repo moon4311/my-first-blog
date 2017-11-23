79f6a981c973df3d7c3161bec26bd7fda2acd1bf
kjm4311 <kjm4311@naver.com>
2017-11-23 18:10:18 +0900

    login form

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,61 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth import (REDIRECT_FIELD_NAME, login as auth_login,)
+from django.contrib.auth.views import _get_login_redirect_url
+from django.contrib.auth.forms import  AuthenticationForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.cache import never_cache
+from django.http import HttpResponseRedirect, QueryDict
 from .models import Home, Post, Comment
 from .forms import PostForm, CommentForm
 
 # Create your views here.
 
-def home(request) :
+
+@sensitive_post_parameters()
+@csrf_protect
+@never_cache
+def home(request,
+         template_name='home.html',
+         redirect_field_name=REDIRECT_FIELD_NAME,
+         authentication_form=AuthenticationForm,
+         extra_context=None, redirect_authenticated_user=False
+         ) :
     homes = Home.objects.filter(published_date__lte=timezone.now(   )).order_by('published_date')
-    return render(request, 'home.html',{'homes' : homes})
+
+    redirect_to = request.POST.get(redirect_field_name, request.GET.get(redirect_field_name, ''))
+
+    if redirect_authenticated_user and request.user.is_authenticated:
+        redirect_to = _get_login_redirect_url(request, redirect_to)
+        if redirect_to == request.path:
+            raise ValueError(
+                "Redirection loop for authenticated user detected. Check that "
+                "your LOGIN_REDIRECT_URL doesn't point to a login page."
+            )
+        return HttpResponseRedirect(redirect_to)
+    elif request.method == "POST":
+        form = authentication_form(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return HttpResponseRedirect(_get_login_redirect_url(request, redirect_to))
+    else:
+        form = authentication_form(request)
+
+    current_site = get_current_site(request)
+
+    context = {
+        'form': form,
+        redirect_field_name: redirect_to,
+        'site': current_site,
+        'site_name': current_site.name,
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+
+    return render(request, template_name ,context)
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now(   )).order_by('published_date')

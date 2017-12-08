b8f448272f4baf7d7bfe5dfa1570596d7eb10357
kjm4311 <kjm4311@naver.com>
2017-12-08 14:25:16 +0900

    knn & take_m_tip

---
+++ test/temp.py
@@ -0,0 +1,17 @@
+import cv2
+from PIL import ImageGrab
+import numpy as np
+
+while(True):
+    img = cv2.imread("images/num33.png")
+    # img2=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img2 = ImageGrab.grab(bbox=(492, 620, 512, 640))
+    img2 = np.array(img2)
+    cv2.imshow("frame",img2)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    elif key != 27 :
+        cv2.imwrite('images/num_6.jpg',img2)
+

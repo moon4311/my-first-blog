23fca5234e161cace9835b322f6806c546f0f822
kjm4311 <kjm4311@naver.com>
2017-12-14 10:06:44 +0900

    .

--- test/connector.py
+++ test/connector.py
@@ -2,76 +2,94 @@
 import sqlite3
 from sqlite3 import Error
 
-# db = "test.sqlite3"
-# db = "D://djangogirls/test.sqlite3"
-db = "../db.sqlite3"
 
+class Connector:
 
-def get_database():
-    print("get_database : ", db)
-    return db
+    def __init__(self):
+        self.db = "test.sqlite3"
+        # db = "D://djangogirls/test.sqlite3"
+        # db = "../db.sqlite3"
 
+    def get_database(self):
+        print("get_database : ", self.db)
+        return self.db
 
-def set_database(db):
-    super.db = db
+    def set_database(self, db):
+        self.db = db
 
+    def connect(self):
+        print("connect db : ", self.db)
+        return sqlite3.connect(self.get_database())
 
-def connect():
-    print("connect db : ", db)
-    return sqlite3.connect(get_database())
+    def setting_query_column(self, param):
+        """
+        :param sql:  select , insert into , update 
+        :param param: ("id","name","date" ... )
+        :return: query
+        """
+        param_str = ""
+        if type(self.param) == "str":
+            param_str = "*"
+        else:
+            for column in param:
+                param_str = param_str + " " + column
+                if column != param[len(param)-1]:
+                    param_str = param_str + ","
 
+        return param_str
 
+    def setting_query_param(self, sql, param):
+        """
+        :param sql:  select ~ from  
+        :param param:  {"id" : "1", "name" : "KJM" }
+        :return: query
+        """
+        if self.param == {}:
+            return sql
+        else:
+            sql = sql + " WHERE "
+            while len(param) != 0 :
+                column = param.popitem()
+                sql = sql + column[0] + " = :"+column[0] + " "
+                if len(param) >= 1:
+                    sql = sql + " AND "
+        return sql
 
-def setting_query(sql,param):
-    while len(param) != 0 :
-        column = param.popitem()
-        sql = sql + column[0] + " = :"+column[0] + " "
-        if len(param) >= 1 : sql = sql + " AND "
-    print("query : " , sql)
-    return sql
+    def select_all(self, table, param, column="*"):
+        """
+        :param table: Table Name
+        :param param: Parameter  ex) {"id":"1", "title":"Test"}
+        :param column: ("id","name","data" ...)
+        :return:
+        """
+        conn = self.connect()
+        sql = "SELECT" + self.setting_query_column(column) + " FROM " + table
+        sql = self.setting_query_param(sql, param.copy())
+        cur = conn.cursor()
+        cur.execute(sql, param)
+        rows = cur.fetchall()
+        conn.close()
+        return rows
 
-def select_All(table,param) :
-    """
-    :param table: Table Name
-    :param param: Parameter  ex) (1,'sea')
-    :return:
-    """
-    conn = connect()
-    sql = " SELECT * FROM " + table + " WHERE "
-    c_param = param.copy()
-    sql = setting_query(sql,c_param)
-    cur = conn.cursor()
-    cur.execute(sql,param)
-    rows = cur.fetchall()
-    conn.close()
-    return rows
+    def insert_result(self, table, insert_data):
+        """
+        :param table:  "table Name"
+        :param insert_data: {"id":"a", "Title":"ttt"}
+        :return: boolean
+        """
+        conn = self.connect()
 
-def select_All(table,column,param) :
-    """
-    :param table: Table Name
-    :param param: Parameter  ex) (1,'sea')
-    :return:
-    """
-    conn = connect()
-
-
-    sql = " SELECT "
-    
-    ########################################column
-
-    c_param = param.copy()
-    sql = setting_query(sql,c_param)
-    cur = conn.cursor()
-    cur.execute(sql,param)
-    rows = cur.fetchall()
-    conn.close()
-    return rows
-
-
-
-rows = select_All("blog_post", {'id':'1','text':'Test'})
-print(rows)
-
-def insert_result(table,insert_data):
-    conn = sqlite3.connect(db)
-    sql =  "insert into result(g_id, sequence,result,ex_o,ex_e,o,e,t,img ) " + " "
+        keys = str(tuple(insert_data.keys()))
+        values = str(tuple(insert_data.values()))
+        sql = "INSERT INTO " + table + " " + keys + " VALUES " + values + ";"
+        print("sql : ", sql)
+        try:
+            cur = conn.cursor()
+            test = cur.execute(sql)
+            conn.commit()
+        except Error as e:
+            print("ERROR : ", e)
+            return False
+        finally:
+            conn.close()
+        return True

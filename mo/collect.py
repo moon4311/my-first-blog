ee11ee00e13e4f6eb5181f936c3c432a58a4b4e2
moon4311 <moon4311@gmail.com>
2017-12-27 21:44:56 +0900

    test

--- mo/collect.py
+++ mo/collect.py
@@ -11,8 +11,10 @@ import pc_collect as pc
 # pc.image_read_after_insert("B6", pc.B6) # 15:07
 
 # 20:42 B2 B4 B5 B6 C1 C6 B2 C5 B3 B1
+# 21:20  B5 B4 B6 B7 C2  C6  C1        C3 C2  B3
+
 pc.image_read_after_insert_detail()
 
-# pc.image_read_after_insert("C1", pc.B1) # 22:04
+# pc.image_read_after_insert("C1", pc.B7) # 22:04
 # pc.image_read_after_insert("B7", pc.B3) # 22:04
 # pc.image_read_after_insert("B7", pc.B4) # 22:04
